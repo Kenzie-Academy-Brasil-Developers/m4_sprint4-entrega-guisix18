eb057d1c52a26aa0fedc1dba78a94f673758601e
Guilherme Augusto da Silva <guilhermeshadow91@gmail.com>
2022-05-04 20:49:29 -0300

    feat: Creating products controller

---
+++ src/controllers/products/productsController.js
@@ -0,0 +1,83 @@
+import createProductService from "../../services/products/createProduct.service";
+import deleteProductService from "../../services/products/deleteProduct.service";
+import listOneProductService from "../../services/products/listOneProduct.service";
+import listProductsService from "../../services/products/listProducts.service";
+import updateProductService from "../../services/products/updateProduct.service";
+
+
+export default class ProductsController {
+    async store(request, response) {
+        const {name, price} = request.body;
+
+        try {
+            const product = await createProductService({name, price});
+
+            return response.status(201).json(product);
+        } catch (err) {
+            return response.status(400).json({
+                status: "error",
+                message: err.message
+            });
+        }
+    }
+
+    async index(request, response) {
+        try {
+            const products = await listProductsService();
+
+            return response.json(products);
+        } catch (err) {
+            return response.status(400).json({
+                status: "error",
+                message: err.message
+            });
+        }
+    }
+
+    async show(request, response) {
+        const {id} = request.params;
+
+        try {
+            const oneProduct = await listOneProductService({id});
+
+            return response.status(200).json(oneProduct);
+        } catch (err) {
+            return response.status(400).json({
+                status: "error",
+                message: err.message
+            });
+        }
+    }
+
+    update(request, response) {
+        const {id} = request.params;
+        const {name, price} = request.body;
+
+        try {
+            const updateProduct = updateProductService({name, price, id});
+
+            return response.status(202).json(updateProduct);
+        } catch (err) {
+            return response.status(400).json({
+                status: "error",
+                message: err.message
+            });
+        }
+    }
+
+    delete(request, response) {
+        const {id} = request.params;
+
+        try {
+            const deleteProduct = deleteProductService({id});
+
+            return response.status(204).json(deleteProduct);
+        } catch (err) {
+            return response.status(400).json({
+                status: "error",
+                message: err.message
+            });
+        }
+
+    }
+}
