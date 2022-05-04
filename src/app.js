cdf54231cd46e9be316448a3f3aa4b529f12e327
Guilherme Augusto da Silva <guilhermeshadow91@gmail.com>
2022-05-03 21:01:46 -0300

    feat: Starting the project

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 import express from "express";
 import "dotenv/config";
+import { startDatabase } from "./database";
+import routeCategories from "./routers/categories.routes";
 
 const app = express();
 
 app.use(express.json());
+app.use("/categories", routeCategories);
 
 export default app.listen(3333, () => {
+  startDatabase();
   console.log("Server running");
 });
