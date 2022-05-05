e1264941fc5d317dfdf12f1e14d0a3c551fd298e
Guilherme Augusto da Silva <guilhermeshadow91@gmail.com>
2022-05-05 18:39:55 -0300

    feat: Creating Service get products by category

---
+++ src/services/products/listProductCategory.service.js
@@ -0,0 +1,23 @@
+import database from "../../database";
+
+const listProductCategoryService = async ({id}) => {
+
+    try {
+        const res =  await database.query(
+        "SELECT p.name, p.price, c.name FROM products p INNER JOIN categories c ON c.id = $1",
+        [id]
+        );
+
+        if (!res.rows.length) {
+            throw new Error("ID invalid");
+        }
+
+        return res.rows[0];
+
+    } catch (err) {
+        throw new Error(err);
+    }
+
+}
+
+export default listProductCategoryService;
